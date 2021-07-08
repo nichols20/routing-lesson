65d07ea4fcb2e0dfb1af1b871d74414cd216c02d
nichols20 <johnnytes10@gmail.com>
2021-07-08 13:17:50 -0400

    programmatic navigation

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -3,6 +3,16 @@ import React, { Component } from "react";
 class ProductDetails extends Component {
   handleSave = () => {
     // Navigate to /products
+    this.props.history.push('/products')
+    //this.props.history.replace('/products')
+
+    /* The two elements above represent an implementation of programmatic navigation. when a user clicks the save button
+    on the product details page the method this.props.history.push will send the user to the products page then adds
+    the previous page to it's history so that a user can press the back button to return to the previous page visited. 
+    the .replace method will replace the previously visited url with prducts page url which will make the user unable
+    to return to the previous page visited. This method is useful for login pages so users don't have to go back to a log
+    in page after already logging in. */ 
+
   };
 
   render() {
@@ -16,3 +26,5 @@ class ProductDetails extends Component {
 }
 
 export default ProductDetails;
+
+
